b9b030ba10bd4c83cc0323fd9674e056aafcaca2
davifm <davifm@gmail.com>
2017-09-28 19:45:30 -0300

    Primeiro commit \o/

---
+++ src/main/java/com/wade/hotsys/HotSysProjectApplication.java
@@ -0,0 +1,12 @@
+package com.wade.hotsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotSysProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotSysProjectApplication.class, args);
+	}
+}
